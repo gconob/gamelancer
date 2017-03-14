be2ebcadffaa2e4681942f08f44d7136e9c6b3d4
Jeong Hwan Kim <jhkim@lotussoft.co.kr>
2017-03-14 10:56:54 +0900

    change

--- gamelancer_main/views.py
+++ gamelancer_main/views.py
@@ -7,7 +7,7 @@ from django.contrib import auth
 from django.core.context_processors import csrf
 from gamelancer_main.forms import *
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile
+from .models import Profile
 
 #pos key : U01TX0FVVEgyMDE3MDMxMjAyMTcxNTE5NTk4 
 import pdb
@@ -26,14 +26,15 @@ def auth_view(request):
     user = auth.authenticate(username=username, password=password)
     if user is not None:
         if user.is_active:
-            auth.login(request, user)            
-            userprofile = UserProfile(user=user)
-            request.session['user_id']=user.id
-            request.session['user_name']=user.username
-            if (userprofile.usertype  == '0') or (userprofile.usertype == 0):
+            auth.login(request, user)
+            u = User.objects.get(id=user.id)
+            usertype = u.profile.usertype;           
+            if (usertype  == 0):
                 return HttpResponseRedirect('/client/main')
-            if (userprofile.usertype == '1') or (userprofile.usertype== 1):
+            if (usertype == 1):
                 return HttpResponseRedirect('/partner/main')    
+    else:
+        return login(request)
   #  c={}
   #  c.update(csrf(request))
   #  return render_to_response('gamelancer_main/login.html', c)
@@ -46,7 +47,7 @@ def client_main(request):
 
 @login_required(login_url='/accounts/login/')
 def partner_main(request):
-    return render(request, 'gamelancer_main/parter_main.html')
+    return render(request, 'gamelancer_main/partner_main.html')
 
 def register(request):  
     if request.method=='POST':
